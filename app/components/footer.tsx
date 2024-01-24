3b1a64f8a4aa0979b21dd047e7e487d7fdddfcb7
Sina Pirani <sinapiranidev@gmail.com>
2024-01-24 18:59:54 +0330

    style: change footer email input to green

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
                 />
       
                 <button
-                  className="absolute end-1 top-1/2 -translate-y-1/2 rounded-full bg-blue-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-blue-700"
+                  className="absolute end-1 top-1/2 -translate-y-1/2 rounded-full bg-green-500 px-5 py-3 text-sm font-medium text-white transition hover:bg-green-700"
                 >
                   عضویت
                 </button>
